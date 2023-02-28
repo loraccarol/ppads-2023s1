e015042b9fd73e7876a8f6c5b5f6b99f8332b2bb
Carolina Carvalho <carolina.dossantos0806@gmail.com>
2023-02-27 23:38:48 -0300

    Adicionando tabela grade horária

---
+++ src/escola-octogono-back/src/main/java/com/api/escolaoctogono/service/model/GradeHorarios.java
@@ -0,0 +1,47 @@
+package com.api.escolaoctogono.service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class GradeHorarios {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    private Professor professor;
+
+    @ManyToOne
+    private Disciplina disciplina;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+
+    public Disciplina getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(Disciplina disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    
+}
