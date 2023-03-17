5d01af2a540a0869c6672fd68ebec2ea88f0d6fd
Carolina Carvalho <carolina.dossantos0806@gmail.com>
2023-03-17 11:34:47 -0300

    crud de turmas

--- src/escola-octogono-back/src/main/java/com/api/escolaoctogono/controller/TurmaController.java
+++ src/escola-octogono-back/src/main/java/com/api/escolaoctogono/controller/TurmaController.java
@@ -1,12 +1,20 @@
 package com.api.escolaoctogono.controller;
 
+import javax.management.relation.RoleNotFoundException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.escolaoctogono.service.core.TurmaService;
+import com.api.escolaoctogono.service.model.Turma;
 
 @RestController
 @RequestMapping("/turmas")
@@ -19,9 +27,29 @@ public class TurmaController {
     }
 
     @GetMapping(value = "/", produces = {"application/json"})
-    public ResponseEntity getTurmas() {
-
+    public ResponseEntity<Object> getTurmas() {
         return new ResponseEntity<>(turmaService.getAllTurmas(), HttpStatus.OK);
+    }
+
+    @PostMapping("/criar")
+    public ResponseEntity<Turma> criar(@RequestBody Turma turma) {
+
+        Turma turmas = turmaService.adicionaTurma(turma);
+
+        return new ResponseEntity<>(turmas, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/turma/{turmaId}")
+    public ResponseEntity<Turma> update(@PathVariable("turmaId") Long turmaId,
+            @RequestBody Turma turma) throws RoleNotFoundException {
+        Turma novaTurma = turmaService.atualizaTurma(turma);
+        return new ResponseEntity<>(novaTurma, HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/turma/{turmaId}")
+    public ResponseEntity<HttpStatus> delete(@PathVariable("turmaId") long turmaId) {
+        turmaService.deleteTurma(turmaId);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
 }
--- src/escola-octogono-back/src/main/java/com/api/escolaoctogono/service/core/TurmaService.java
+++ src/escola-octogono-back/src/main/java/com/api/escolaoctogono/service/core/TurmaService.java
@@ -1,6 +1,9 @@
 package com.api.escolaoctogono.service.core;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.management.relation.RoleNotFoundException;
 
 import org.springframework.stereotype.Service;
 
@@ -20,5 +23,29 @@ public class TurmaService {
     public List<Turma> getAllTurmas(){
         return turmaRepository.findAll();
     }
-    
+
+    public Optional<Turma> getTurmaById(Long turmaId){
+        return turmaRepository.findById(turmaId);
+    }
+
+    public Turma adicionaTurma(Turma turma) {
+        return turmaRepository.save(turma);
+    }
+
+    public Turma atualizaTurma(Turma turma) throws RoleNotFoundException  {
+
+        Turma atuTurma = turmaRepository.findById(turma.getId())
+        .orElseThrow(() -> new RoleNotFoundException("Não foi possível encontrar essa turma"));
+
+        atuTurma.setAno(turma.getAno());
+        atuTurma.setCodigo(turma.getCodigo());
+
+        final Turma novaTurma = turmaRepository.save(atuTurma);
+        return novaTurma;
+    }
+
+    public void deleteTurma(long turmaId) {
+        turmaRepository.deleteById(turmaId);
+    }
+
 }
