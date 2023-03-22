fa2c5a084cd6499d2a87205816c76fe315a262f5
Carolina Carvalho <carolina.dossantos0806@gmail.com>
2023-03-22 19:07:51 -0300

    mudando css para ficar tudo universal

--- src/missed-class-front/src/App.js
+++ src/missed-class-front/src/App.js
@@ -3,6 +3,7 @@ import Login from './pages/Login.js'
 import Turmas from './pages/Turmas.js'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Gerenciamento from './pages/Gerenciamento';
+import NaoDisponivel from './pages/NaoDisponivel';
 
 function App() {
   return (
@@ -10,11 +11,11 @@ function App() {
       <div className='App'>
         <Routes>
           <Route exact path='/' element={<Login />} />
-          <Route path='/turmas' element={<Turmas />} />
+          <Route path='/turmas' element={<NaoDisponivel />} />
           <Route path='/gerenciamento' element={<Gerenciamento />} />
+          <Route path='/*' element={<NaoDisponivel />} />
         </Routes>
       </div>
-
     </Router>
   );
 }
