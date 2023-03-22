1858bc123b1d580f33a2ac74022dfc5cd6020d14
Carolina Carvalho <carolina.dossantos0806@gmail.com>
2023-03-22 19:09:50 -0300

    criando pagina para paginas nao disponiveis

---
+++ src/missed-class-front/src/pages/NaoDisponivel.js
@@ -0,0 +1,9 @@
+import { Grid, Text } from "@chakra-ui/react";
+
+export default function NaoDisponivel(){
+    return (
+        <Grid height={"100vh"} placeItems={"center"}>
+            <Text>Essa página não existe.</Text>
+        </Grid>
+    )
+}
