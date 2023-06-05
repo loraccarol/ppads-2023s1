98e36acfada948256cdbacf0d70b5d6da53a4f43
Carolina Carvalho <carolina.dossantos0806@gmail.com>
2023-06-05 00:35:29 -0300

    link back no front

--- src/missed-class-front/src/service/api.js
+++ src/missed-class-front/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: "https://ppads-2023s1-escola-octogono.up.railway.app/"
+    baseURL: "https://ppads-2023s1-production-2727.up.railway.app/"
 })
 
